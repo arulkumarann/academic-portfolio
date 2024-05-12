ffa7460533342222cb15744a3b661641c9930b3e
Arul Kumaran P <arulkumaranp02@gmail.com>
2024-05-12 14:26:08 +0530

    update username

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 
 	export let data, darkMode;
 
-	const githubLink = 'https://github.com/vaishnav-mk/academic-portfolio';
+	const githubLink = 'https://github.com/arulkumarann/academic-portfolio';
 </script>
 
 <footer
